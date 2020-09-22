b5e9da4208dcca07c2585f8402607d0b5daf158d
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-09-22 18:13:37 +0900

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,64 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <input v-model="todoState.input">
+  <button @click="addTodo">add todo</button>
+
+  <div>todos</div>
+  <ul>
+    <li v-for="{value, id} in todoState.todos" :key="id">{{value}}<button @click="deleteTodo(id)">X</button><button @click="doneTodo(id)">O</button></li>
+  </ul>
+  <div>done</div>
+  <ul>
+    <li v-for="{value, id} in todoState.doneTodos" :key="id">{{value}}<button @click="notDoneTodo(id)">X</button></li>
+  </ul>
 </template>
 
 <script>
+import {computed, reactive} from 'vue'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
+  setup() {
+    const [todoState] = useTodo()
+    const [addTodo, deleteTodo, doneTodo, notDoneTodo] = setTodo(todoState)
     return {
-      count: 0
+      todoState,
+      addTodo,
+      deleteTodo,
+      doneTodo,
+      notDoneTodo
     }
   }
+}
+
+function useTodo(){
+  const todoState = reactive({
+    todos: [{value: 'sample', id: 0}],
+    input: '',
+    id: 1,
+    doneTodos: []
+  })
+  return [todoState]
+}
+
+function setTodo(todoState){
+  function addTodo(){
+    todoState.id ++
+    todoState.todos.push({value: todoState.input, id: todoState.id})
+    todoState.input = ''
+  }
+
+  function deleteTodo(id){
+    todoState.todos = todoState.todos.filter(todo=>todo.id !== id)
+  }
+
+  function doneTodo(id){
+    todoState.doneTodos = todoState.todos.filter(todo=>todo.id === id)
+    todoState.todos = todoState.todos.filter(todo=>todo.id !== id)
+  }
+
+  function notDoneTodo(id){
+    todoState.todos = todoState.doneTodos.filter(todo=>todo.id === id)
+    todoState.doneTodos = todoState.doneTodos.filter(todo=>todo.id !== id)
+  }
+  return [addTodo, deleteTodo, doneTodo, notDoneTodo]
 }
 </script>
